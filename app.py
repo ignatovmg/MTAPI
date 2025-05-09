adc372f9e44fa03771a0e5cb8c314dcf9f3edce3
Mike Myers <36300408-ignatovmg@users.noreply.replit.com>
2025-05-09 18:55:51 +0000

    Add host and port to settings

--- app.py
+++ app.py
@@ -18,12 +18,10 @@ import logging
 import os
 
 app = Flask(__name__)
-app.config.update(
-    MAX_TRAINS=10,
-    MAX_MINUTES=30,
-    CACHE_SECONDS=60,
-    THREADED=True
-)
+app.config.update(MAX_TRAINS=10,
+                  MAX_MINUTES=30,
+                  CACHE_SECONDS=60,
+                  THREADED=True)
 
 _SETTINGS_ENV_VAR = 'MTAPI_SETTINGS'
 _SETTINGS_DEFAULT_PATH = './settings.cfg'
@@ -32,14 +30,19 @@ if _SETTINGS_ENV_VAR in os.environ:
 elif os.path.isfile(_SETTINGS_DEFAULT_PATH):
     app.config.from_pyfile(_SETTINGS_DEFAULT_PATH)
 else:
-    raise Exception('No configuration found! Create a settings.cfg file or set MTAPI_SETTINGS env variable.')
+    raise Exception(
+        'No configuration found! Create a settings.cfg file or set MTAPI_SETTINGS env variable.'
+    )
 
 # set debug logging
 if app.debug:
-    logging.basicConfig(level=logging.INFO,
+    logging.basicConfig(
+        level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
+
 class CustomJSONEncoder(json.JSONEncoder):
+
     def default(self, obj):
         try:
             if isinstance(obj, datetime):
@@ -51,26 +54,26 @@ class CustomJSONEncoder(json.JSONEncoder):
             return list(iterable)
         return JSONEncoder.default(self, obj)
 
-mta = Mtapi(
-    app.config['MTA_KEY'],
-    app.config['STATIONS_FILE'],
-    max_trains=app.config['MAX_TRAINS'],
-    max_minutes=app.config['MAX_MINUTES'],
-    expires_seconds=app.config['CACHE_SECONDS'],
-    threaded=app.config['THREADED'])
+
+mta = Mtapi(app.config['MTA_KEY'],
+            app.config['STATIONS_FILE'],
+            max_trains=app.config['MAX_TRAINS'],
+            max_minutes=app.config['MAX_MINUTES'],
+            expires_seconds=app.config['CACHE_SECONDS'],
+            threaded=app.config['THREADED'])
+
 
 def response_wrapper(f):
+
     @wraps(f)
     def decorated_function(*args, **kwargs):
         resp = f(*args, **kwargs)
 
         if not isinstance(resp, Response):
             # custom JSON encoder; this is important
-            resp = Response(
-                response=json.dumps(resp, cls=CustomJSONEncoder),
-                status=200,
-                mimetype="application/json"
-            )
+            resp = Response(response=json.dumps(resp, cls=CustomJSONEncoder),
+                            status=200,
+                            mimetype="application/json")
 
         add_cors_header(resp)
 
@@ -78,13 +81,16 @@ def response_wrapper(f):
 
     return decorated_function
 
+
 def add_cors_header(resp):
     if app.config['DEBUG']:
         resp.headers['Access-Control-Allow-Origin'] = '*'
     elif 'CROSS_ORIGIN' in app.config:
-        resp.headers['Access-Control-Allow-Origin'] = app.config['CROSS_ORIGIN']
+        resp.headers['Access-Control-Allow-Origin'] = app.config[
+            'CROSS_ORIGIN']
 
     return resp
+
 
 @app.route('/')
 @response_wrapper
@@ -92,7 +98,8 @@ def index():
     return {
         'title': 'MTAPI',
         'readme': 'Visit https://github.com/jonthornton/MTAPI for more info'
-        }
+    }
+
 
 @app.route('/by-location', methods=['GET'])
 @response_wrapper
@@ -101,35 +108,35 @@ def by_location():
         location = (float(request.args['lat']), float(request.args['lon']))
     except KeyError as e:
         print(e)
-        resp = Response(
-            response=json.dumps({'error': 'Missing lat/lon parameter'}),
-            status=400,
-            mimetype="application/json"
-        )
+        resp = Response(response=json.dumps(
+            {'error': 'Missing lat/lon parameter'}),
+                        status=400,
+                        mimetype="application/json")
 
         return add_cors_header(resp)
 
     data = mta.get_by_point(location, 5)
     return _make_envelope(data)
 
+
 @app.route('/by-route/<route>', methods=['GET'])
 @response_wrapper
 def by_route(route):
 
     if route.islower():
-        return redirect(request.host_url + 'by-route/' + route.upper(), code=301)
+        return redirect(request.host_url + 'by-route/' + route.upper(),
+                        code=301)
 
     try:
         data = mta.get_by_route(route)
         return _make_envelope(data)
     except KeyError as e:
-        resp = Response(
-            response=json.dumps({'error': 'Station not found'}),
-            status=404,
-            mimetype="application/json"
-        )
+        resp = Response(response=json.dumps({'error': 'Station not found'}),
+                        status=404,
+                        mimetype="application/json")
 
         return add_cors_header(resp)
+
 
 @app.route('/by-id/<id_string>', methods=['GET'])
 @response_wrapper
@@ -139,21 +146,18 @@ def by_index(id_string):
         data = mta.get_by_id(ids)
         return _make_envelope(data)
     except KeyError as e:
-        resp = Response(
-            response=json.dumps({'error': 'Station not found'}),
-            status=404,
-            mimetype="application/json"
-        )
+        resp = Response(response=json.dumps({'error': 'Station not found'}),
+                        status=404,
+                        mimetype="application/json")
 
         return add_cors_header(resp)
+
 
 @app.route('/routes', methods=['GET'])
 @response_wrapper
 def routes():
-    return {
-        'data': sorted(mta.get_routes()),
-        'updated': mta.last_update()
-        }
+    return {'data': sorted(mta.get_routes()), 'updated': mta.last_update()}
+
 
 def _envelope_reduce(a, b):
     if a['last_update'] and b['last_update']:
@@ -163,15 +167,16 @@ def _envelope_reduce(a, b):
     else:
         return b
 
+
 def _make_envelope(data):
     time = None
     if data:
         time = reduce(_envelope_reduce, data)['last_update']
 
-    return {
-        'data': data,
-        'updated': time
-    }
+    return {'data': data, 'updated': time}
+
 
 if __name__ == '__main__':
-    app.run(use_reloader=False)
+    app.run(use_reloader=False,
+            host=app.config['HOST'],
+            port=int(app.config['PORT']))
